716798066642fd219ecd8be871bd1b953f05b75a
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-18 23:04:32 +0900

    記事一覧表示機能の実装完了

--- backend/resources/views/includes/thread_list.blade.php
+++ backend/resources/views/includes/thread_list.blade.php
@@ -1,73 +1,30 @@
 <section class="top-list-section mt-5">
-    <div class="list-group w-75 mx-auto">
-        <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small>3comments</small>
-            </div>
-            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus
-                varius blandit.</p>
-            <small>Donec id elit non mi porta.</small>
-        </a>
-        <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small class="text-muted">213ccomments</small>
-            </div>
-            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus
-                varius blandit.</p>
-            <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-        <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small class="text-muted">0comments</small>
-            </div>
-            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus
-                varius blandit.</p>
-            <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-        <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small>3comments</small>
-            </div>
-            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus
-                varius blandit.</p>
-            <small>Donec id elit non mi porta.</small>
-        </a>
-        <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small class="text-muted">5comments</small>
-            </div>
-            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus
-                varius blandit.</p>
-            <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-        <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">List group item heading</h5>
-                <small class="text-muted">3 comments</small>
-            </div>
-            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus
-                varius blandit.</p>
-            <small class="text-muted">Donec id elit non mi porta.</small>
-        </a>
-        <nav class="mx-auto mt-3">
-            <ul class="pagination">
-                <li class="page-item disabled">
-                    <a class="page-link" href="#!" tabindex="-1">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#!">1</a></li>
-                <li class="page-item active">
-                    <a class="page-link" href="#!">2 <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#!">3</a>
-                <li class="page-item">
-                    <a class="page-link" href="#!">Next</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
+  <div class="list-group w-75 mx-auto">
+    @foreach ($data as $item)
+    <a href="#!" class="list-group-item list-group-item-action flex-column align-items-start">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">{{ $item->title }}</h5>
+        <small>コメント数:3件</small>
+      </div>
+      <p class="mb-1">{{ $item->content }}</p>
+      <small>{{ $item->created_at }}</small>
+    </a>
+    @endforeach
+    {{ $data->links() }}
+    <nav class="mx-auto mt-3">
+      <ul class="pagination">
+        <li class="page-item disabled">
+          <a class="page-link" href="#!" tabindex="-1">Previous</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#!">1</a></li>
+        <li class="page-item active">
+          <a class="page-link" href="#!">2 <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#!">3</a>
+        <li class="page-item">
+          <a class="page-link" href="#!">Next</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </section>
