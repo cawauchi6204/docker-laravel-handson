78a48d435bff46f474bbcca28fda280a27300d7e
cawauchi6204 <cawauchi6204@gmail.com>
2020-11-23 17:49:29 +0900

    インデントの修正

--- backend/resources/views/includes/comment_form.blade.php
+++ backend/resources/views/includes/comment_form.blade.php
@@ -3,8 +3,9 @@
     @csrf
     <div class="form-group w-75 mx-auto">
       <label for="formGroupExampleInput2">コメント内容</label>
-      <textarea rows="5" cols="35" class="form-control" id="formGroupExampleInput2" placeholder="内容" name="content"></textarea>
-      <input type="hidden" name="posts_id" value={{ $posts_data->id }} />
+      <textarea rows="5" cols="35" class="form-control" id="formGroupExampleInput2" placeholder="内容"
+        name="content"></textarea>
+      <input type="hidden" name="posts_id" value={{ $post->id }} />
       <input class="btn btn-success mt-3" type="submit" value="新規コメント書き込み">
     </div>
   </form>
