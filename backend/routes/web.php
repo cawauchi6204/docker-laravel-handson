7d7b47f5eaa7f16646a503f5eabaa1acdf82a3ac
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-02 21:58:20 +0900

    フォーマットにてスペースを修正

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostsController::class,'index']);
+Route::get('/', [PostsController::class, 'index']);
 
 Route::prefix('post')->group(function () {
-  Route::post('/store', [PostsController::class,'store'])->name('post.store');
-  Route::get('/{id}',[PostsController::class,'detail'])->name('post.detail');
+    Route::post('/store', [PostsController::class, 'store'])->name('post.store');
+    Route::get('/{id}', [PostsController::class, 'detail'])->name('post.detail');
 });
 
-Route::post('comment/store',[CommentsController::class,'store'])->name('comment.store');
+Route::post('comment/store', [CommentsController::class, 'store'])->name('comment.store');
