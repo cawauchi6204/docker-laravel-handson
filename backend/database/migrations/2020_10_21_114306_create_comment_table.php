a61bac102d3f30e701fedb5fe8f17208d417f109
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-02 22:06:31 +0900

    fix
    カラム名をposts_idからpost_idという単数系に修正

--- backend/database/migrations/2020_10_21_114306_create_comment_table.php
+++ backend/database/migrations/2020_10_21_114306_create_comment_table.php
@@ -16,7 +16,7 @@ class CreateCommentTable extends Migration
         Schema::create('comment', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->nullable();
-            $table->integer('posts_id');
+            $table->integer('post_id');
             $table->longtext('content');
             $table->boolean('deleted')->default(false);
             $table->timestamps();
