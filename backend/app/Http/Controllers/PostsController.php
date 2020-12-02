541f950e3b85a94976879a5346cf1ec8860a892c
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-02 22:08:55 +0900

    fix
    フォーマットによるインデントの修正と
    必要のない名前空間の削除と変数の削除

--- backend/app/Http/Controllers/PostsController.php
+++ backend/app/Http/Controllers/PostsController.php
@@ -2,41 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-  public function index()
-  {
-    $data = Post::paginate(10);
-    return view('index',compact('data'));
-  }
+    public function index()
+    {
+        $data = Post::paginate(10);
+        return view('index', compact('data'));
+    }
 
-  public function store(Request $request)
-  {
-    $post = new Post();
+    public function store(Request $request)
+    {
+        $post = new Post();
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->save();
+        return redirect()->route('post.detail', ['id' => $post->id]);
+    }
 
-    $post->title = $request->title;
-    $post->content = $request->content;
-
-    $post->save();
-    $id = $post->id;
-    return redirect()->route('post.detail',['id' => $id]);
-  }
-
-  public function detail(Request $request,$id)
-  {
-    $post = Post::where('id',$id)->first();
-
-    $comment = Comment::getCommentById($id);
-
-    return view('posts.{id}',[
-      'post' => $post,
-      'comment' => $comment
-      ]);
-  }
+    public function detail($id)
+    {
+        $post = Post::where('id', $id)->first();
+        $comment = Comment::getCommentById($id);
+        return view('posts.{id}', [
+            'post' => $post,
+            'comment' => $comment,
+        ]);
+    }
 }
