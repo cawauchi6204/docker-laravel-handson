d469f142ce10c26d12d55f7779ceb06f29371254
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-02 22:00:20 +0900

    fix
    必要のない名前空間の削除と必要のない変数の削除、
    posts_idをpost_idという単数系に修正

--- backend/app/Http/Controllers/CommentsController.php
+++ backend/app/Http/Controllers/CommentsController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 use App\Models\Comment;
+use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-  public function store(Request $request)
-  {
-    $comment = new Comment();
-
-    $comment->content = $request->content;
-    $posts_id = $request->posts_id;
-    $comment->posts_id = $posts_id;
-    $comment->save();
-    return redirect()->route('post.detail', ['id' =>$posts_id]);
-  }
+    public function store(Request $request)
+    {
+        $comment = new Comment();
+        $comment->content = $request->content;
+        $comment->post_id = $request->post_id;
+        $comment->save();
+        return redirect()->route('post.detail', ['id' => $comment->post_id]);
+    }
 }
