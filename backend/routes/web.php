7deb6c25a9db2b4142dac25b1b9308a83730e242
cawauchi6204 <cawauchi6204@gmail.com>
2020-12-02 21:03:31 +0900

    fix(backend/router/web.php): 別々で管理していたRouteをprefixを使ってまとめた
    #14

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -6,8 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class,'index']);
 
-Route::post('post/store', [PostsController::class,'store'])->name('post.store');
-
-Route::get('post/{id}',[PostsController::class,'detail'])->name('post.detail');
+Route::prefix('post')->group(function () {
+  Route::post('/store', [PostsController::class,'store'])->name('post.store');
+  Route::get('/{id}',[PostsController::class,'detail'])->name('post.detail');
+});
 
 Route::post('comment/store',[CommentsController::class,'store'])->name('comment.store');
